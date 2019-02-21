c84c3899e1f36d0a010357d15a0e8f2b156756fe
Chandra Delon <34028244+vinix997@users.noreply.github.com>
2019-02-21 10:30:32 +0700

    way
    waypoint edit

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -4,9 +4,24 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour {
 
-    [SerializeField]private float speed;
+    
+    
 
     public int health;
+
+    // Array of waypoints to walk from one to the next one
+    [SerializeField]
+    private Transform[] waypoints;
+
+    // Walk speed that can be set in Inspector
+    [SerializeField]
+    private float moveSpeed = 2f;
+
+    // Index of current waypoint from which Enemy walks
+    // to the next one
+    private int waypointIndex = 0;
+
+    
 
 	// Use this for initialization
 	void Start () {
@@ -14,13 +29,32 @@ public class Enemy : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Translate(Vector2.right * Time.deltaTime * speed);
+       // transform.Translate(Vector2.right * Time.deltaTime * speed);
+        Move();
         if(health <= 0)
         {
             Destroy(this.gameObject);
         }
 	}
+    private void Move()
+    {
 
+        if (waypointIndex <= waypoints.Length - 1)
+        {
+
+
+            transform.position = Vector2.MoveTowards(transform.position,
+               waypoints[waypointIndex].transform.position,
+               moveSpeed * Time.deltaTime);
+
+
+            if (transform.position == waypoints[waypointIndex].transform.position)
+            {
+                waypointIndex += 1;
+
+            }
+        }
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Bullet")
