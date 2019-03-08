f3f7aee8730c79cb7a23d8808b4efffe945955a8
Crouston <yosuam34@gmail.com>
2019-03-08 13:30:26 +0700

    aoe tower edit

--- Assets/Script/AoE_Bullet.cs
+++ Assets/Script/AoE_Bullet.cs
@@ -6,30 +6,30 @@ public class AoE_Bullet : MonoBehaviour {
 
     public Transform enemy;
     private Vector2 target;
-    private float speed;
-    private float timer = 0;
+    //private float speed;
+    private float timer;
     [SerializeField]
     private int vanishTime;
     // Use this for initialization
     void Start()
     {
+        timer = 0;
         enemy = GameObject.FindGameObjectWithTag("Enemy").transform;
         target = new Vector2(enemy.position.x, enemy.position.y);
-        speed = 10f;
-
+        //speed = 10f;
+        transform.position = target;
     }
 
     // Update is called once per frame
     void Update()
     {
         //Spawn di lokasi target 
-        transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+        //transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
         //Utk timer bullet menghilang jika lebih dari 6 detik
         timer += Time.deltaTime;
         if (timer >= vanishTime)
         {
             Destroy(gameObject);
-            timer = 0;
         }
 
 
