e42f5698e352e7d5f14fb008fe18f5ebf4a1bae4
Crouston <yosuam34@gmail.com>
2019-03-22 20:01:09 +0700

    again

--- Assets/Script/AoE_Tower.cs
+++ Assets/Script/AoE_Tower.cs
@@ -34,7 +34,7 @@ public class AoE_Tower : MonoBehaviour {
         attackCooldown = 3f;
         nameButton.GetComponent<NameToggler>().towerName = Instantiate(towerName, transform.position + new Vector3(0, 10), Quaternion.identity, transform);
         upgradeButton.GetComponent<UpgradeButton>().tower = gameObject;
-        nameButton.GetComponent<NameToggler>().upgradeButton = Instantiate(upgradeButton, transform.position+new Vector3(10,-10), Quaternion.identity, transform);
+        nameButton.GetComponent<NameToggler>().upgradeButton = Instantiate(upgradeButton, transform.position+new Vector3(8,-8), Quaternion.identity, transform);
         Instantiate(nameButton, transform.position, Quaternion.identity, transform);
     }
 
